e5416c340042d0dfe2d6e07fd6a5a9a7425b832a
YJ Park <yjpark@gmail.com>
2017-07-14 16:32:32 +0800

    add json helper

---
+++ src/YJPark/Json.elm
@@ -0,0 +1,184 @@
+module YJPark.Json exposing (..)
+
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Dict
+
+
+type alias Data = (Dict.Dict String Encode.Value)
+
+
+empty : Data
+empty =
+    Dict.empty
+
+
+type alias Value = Encode.Value
+null = Encode.null
+
+
+insertBool : String -> Bool -> Data -> Data
+insertBool key val =
+    Dict.insert key (Encode.bool val)
+
+
+insertInt : String -> Int -> Data -> Data
+insertInt key val =
+    Dict.insert key (Encode.int val)
+
+
+insertFloat : String -> Float -> Data -> Data
+insertFloat key val =
+    Dict.insert key (Encode.float val)
+
+
+insertString : String -> String -> Data -> Data
+insertString key str =
+    Dict.insert key (Encode.string str)
+
+
+insertValue : String -> Value -> Data -> Data
+insertValue key val =
+    Dict.insert key val
+
+
+insertOptionalString : String -> String -> Data -> Data
+insertOptionalString key str =
+    if (str /= "") then
+        insertString key str
+    else
+        identity
+
+
+insertOptionalValue : String -> Value -> Data -> Data
+insertOptionalValue key val =
+    if (val /= null) then
+        insertValue key val
+    else
+        identity
+
+
+{--
+insert : String -> (WxDataType, a) -> Data -> Data
+insert key (type_, value) =
+    case type_ of
+        WxBool ->
+            insertBool key value
+        WxInt ->
+            insertInt key value
+        WxFloat ->
+            insertFloat key value
+        WxString ->
+            insertString key value
+        WxData ->
+            insertData key value
+--}
+
+
+dataToValue : Data -> Value
+dataToValue data =
+    Dict.toList data
+        |> Encode.object
+
+
+valueToData : Value -> Data
+valueToData value =
+    if value == null then
+        empty
+    else
+        case Decode.decodeValue (Decode.dict Decode.value) value of
+            Ok result ->
+                result
+            Err err ->
+                Debug.log ("valueToData: Decode Failed: " ++ err) empty
+
+
+stringToValue : String -> Value
+stringToValue str =
+    Encode.string str
+
+
+listToValue : List Value -> Value
+listToValue list =
+    list
+        |> Encode.list
+
+
+asString : Value -> String
+asString val =
+    case (Decode.decodeValue Decode.string val) of
+        Ok val ->
+            val
+        Err err ->
+            err
+
+
+getStringWithDefault : String -> String -> Value -> String
+getStringWithDefault key default val =
+    case (Decode.decodeValue (Decode.field key Decode.string) val) of
+        Ok val ->
+            val
+        Err err ->
+            default
+
+
+getString : String -> Value -> String
+getString key val =
+    getStringWithDefault key "" val
+
+
+getIntWithDefault : String -> Int -> Value -> Int
+getIntWithDefault key default val =
+    case (Decode.decodeValue (Decode.field key Decode.int) val) of
+        Ok val ->
+            val
+        Err err ->
+            default
+
+
+getInt : String -> Value -> Int
+getInt key val =
+    getIntWithDefault key 0 val
+
+
+getFloatWithDefault : String -> Float -> Value -> Float
+getFloatWithDefault key default val =
+    case (Decode.decodeValue (Decode.field key Decode.float) val) of
+        Ok val ->
+            val
+        Err err ->
+            default
+
+
+getFloat : String -> Value -> Float
+getFloat key val =
+    getFloatWithDefault key 0 val
+
+
+getBoolWithDefault : String -> Bool -> Value -> Bool
+getBoolWithDefault key default val =
+    case (Decode.decodeValue (Decode.field key Decode.bool) val) of
+        Ok val ->
+            val
+        Err err ->
+            default
+
+
+getBool : String -> Value -> Bool
+getBool key val =
+    getBoolWithDefault key False val
+
+
+getValueWithDefault : String -> Value -> Value -> Value
+getValueWithDefault key default val =
+    case (Decode.decodeValue (Decode.field key Decode.value) val) of
+        Ok val ->
+            val
+        Err err ->
+            default
+
+
+getValue : String -> Value -> Value
+getValue key val =
+    getValueWithDefault key null val
+
