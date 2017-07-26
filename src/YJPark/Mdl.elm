15252d758587a7d4be0ce940761801f559308a62
YJ Park <yjpark@gmail.com>
2017-07-26 11:38:44 +0800

    more index update helpers

--- src/YJPark/Mdl.elm
+++ src/YJPark/Mdl.elm
@@ -6,6 +6,8 @@ import Native.Mdl
 
 import Material
 import Html exposing (Html)
+import Dict
+
 
 -- Events
 type alias Events obj msg = Events.Type obj msg
@@ -49,6 +51,24 @@ setSubIndex : Index -> Int -> WithIndex m -> WithIndex m
 setSubIndex parent index model =
     model
         |> updateIndex (parent ++ [index])
+
+
+setListIndexes : Index -> List (WithIndex m) -> List (WithIndex m)
+setListIndexes parent subs =
+    subs
+        |> List.indexedMap (setSubIndex parent)
+
+
+setDictIndexes : Index -> Dict.Dict String (WithIndex m) -> Dict.Dict String (WithIndex m)
+setDictIndexes parent subs =
+    let
+        updateSubIndex = \index (key, sub) ->
+            (key, setSubIndex parent index sub)
+    in
+        subs
+            |> Dict.toList
+            |> List.indexedMap updateSubIndex
+            |> Dict.fromList
 
 
 -- Mdl
